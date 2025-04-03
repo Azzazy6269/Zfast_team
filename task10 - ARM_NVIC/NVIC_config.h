8705ce64fd013724cf4982e522ba183347dacf4b
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2025-04-03 04:26:33 +0200

    Add files via upload

---
+++ task10 - ARM_NVIC/NVIC_config.h
@@ -0,0 +1,15 @@
+/********************
+Author    : Mohammad Ibraheem Fatehy
+Date      : 29 Feb 2025
+File_name : NVIC_config.h
+*********************/
+
+#ifndef NVIC_CONFIG_H_
+#define NVIC_CONFIG_H_
+
+#define NUMBER_OF_INTERRUPTS            67
+#define NVIC_MAX_PRIORITY               15
+#define PRIORITY_GROUPING   NVIC_4GROUP_4SUB
+
+
+#endif
